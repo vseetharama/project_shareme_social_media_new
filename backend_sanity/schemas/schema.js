445c4276f8df6564b9aa5591902cd236b84c7d59
vseetharama <mugerayaseetharama@gmail.com>
2026-05-16 08:19:52 +0530

    Backend schemas implementation
    - Added comment.js schema for post comments
    - Added pin.js schema for saved pins
    - Added postedBy.js schema for user who posted
    - Added save.js schema for saved items
    - Updated schema.js with all schema exports

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -4,6 +4,11 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import user from './user'
+import pin from './pin'
+import postedBy from './postedBy'
+import comment from './comment'
+import save from './save'
+
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -11,6 +16,6 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    user
+    user,pin,comment,save,postedBy
   ]),
 })
